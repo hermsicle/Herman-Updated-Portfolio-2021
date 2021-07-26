8a46104a8338999fa273eb392f9db9d8d7a255a9
Herman Liu <hermanliu95@gmail.com>
2021-07-25 21:19:48 -0700

    added navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,56 @@
-import React from 'react'
-// import { Nav } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-scroll'
 
 function NavBar() {
-  return (
-    <div className="nav container">
-      <h1> <Link to="home"> Herman <span>Liu</span>  </Link></h1>
-      <div className="nav-items">
-        <p><Link to="services">Services</Link></p>
-        <p><Link to="projects">Projects</Link></p>
-        <p><Link to="contact">Contact</Link></p>        
-      </div>
-      {/* <div className="nav-menu">
 
-      </div> */}
+  const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
+  const [navBar, setNavBar] = useState(false)
+  const [border, setBorder] = useState(false)
+
+  const showButon = () => {
+    if (window.innerWidth <= 768) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  useEffect( ()=> {
+    showButon()
+  }, [])
+
+  const changeBackground = () => {
+    // console.log(window.scrollY)
+    // If the scroll Y is more than height of navbar
+    if (window.scrollY >= 80) {
+      setNavBar(true)
+    } else {
+      setNavBar(false)
+    }
+
+    if (window.scrollY >= 980) {
+      setBorder(true)
+    } else {
+      setBorder(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground)
+
+  return (
+    <div className={navBar ? "nav-bar active" : "nav-bar"}>
+      <div className="nav container">
+        <h1> <Link to="home"> Herman <span>Liu</span>  </Link></h1>
+        <div className="nav-items">
+          <p><Link to="services">Services</Link></p>
+          <p><Link to="projects">Projects</Link></p>
+          <p><Link to="contact">Contact</Link></p>        
+        </div>
+        <div className="nav-menu">
+          <div className="menu-button"></div>
+        </div>
+      </div>
     </div>
   )
 }
